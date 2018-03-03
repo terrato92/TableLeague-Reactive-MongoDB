d589ed9be37de4af2f0ec9a485ff6c23e7905858
terrato92 <kamilmituraa@gmail.com>
2018-03-03 14:28:42 +0100

    Controller commit

---
+++ src/main/java/terrato/springframwork/controller/LeagueController.java
@@ -0,0 +1,10 @@
+package terrato.springframwork.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by onenight on 2018-03-03.
+ */
+@Controller
+public class LeagueController {
+}
