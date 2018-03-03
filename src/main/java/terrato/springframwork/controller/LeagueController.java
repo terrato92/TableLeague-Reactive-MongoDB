952b6070e44006af47530685a0c62ce15385807e
terrato92 <kamilmituraa@gmail.com>
2018-03-03 21:33:34 +0100

    LeagueController and test commit

--- src/main/java/terrato/springframwork/controller/LeagueController.java
+++ src/main/java/terrato/springframwork/controller/LeagueController.java
@@ -1,10 +1,72 @@
 package terrato.springframwork.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import terrato.springframwork.domain.League;
+import terrato.springframwork.service.LeagueService;
+
+import javax.validation.Valid;
 
 /**
  * Created by onenight on 2018-03-03.
  */
+@Slf4j
 @Controller
 public class LeagueController {
+
+    private final LeagueService leagueService;
+
+    public LeagueController(LeagueService leagueService) {
+        this.leagueService = leagueService;
+    }
+
+    @GetMapping
+    @RequestMapping({"", "/", "/index"})
+    public String showLeague(Model model){
+        model.addAttribute("leagues", leagueService.getLeagues());
+
+        return "league/index";
+    }
+
+    @GetMapping
+    @RequestMapping("leagues/{id}/show")
+    public String showLeagueById(@PathVariable String id, Model model) {
+        model.addAttribute("league", leagueService.getLeagueById(Long.valueOf(id)));
+
+        return "league/" + id + "show";
+    }
+
+    @RequestMapping("league/new")
+    public String newLeague(Model model) {
+        model.addAttribute("league", leagueService.addLeague(new League()));
+
+        return "league/show";
+    }
+
+    @GetMapping
+    @RequestMapping("league/{id}/delete")
+    public String deleteLeague(@PathVariable String id){
+        leagueService.deleteLeagueById(Long.valueOf(id));
+        return "redirect:/show";
+    }
+
+    @PostMapping
+    @RequestMapping("league/{id}/update")
+    public String saveOrUpdateLeague(@Valid @ModelAttribute ("league") League league, BindingResult bindingResult){
+
+        if (bindingResult.hasErrors()){
+            bindingResult.getAllErrors().forEach(objectError -> log.debug(objectError.toString()));
+            return "league/leagueform";
+        }
+
+        League league1 = leagueService.addLeague(league);
+
+        return "league/" + league1.getId() + "/show";
+
+    }
+
+
 }
---
+++ src/test/java/terrato/springframwork/controller/LeagueControllerTest.java
@@ -0,0 +1,115 @@
+package terrato.springframwork.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+import terrato.springframwork.domain.League;
+import terrato.springframwork.service.LeagueService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * Created by onenight on 2018-03-03.
+ */
+public class LeagueControllerTest {
+
+    @Mock
+    Model model;
+
+    @Mock
+    LeagueService leagueService;
+
+    LeagueController leagueController;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        leagueController = new LeagueController(leagueService);
+        mockMvc = MockMvcBuilders.standaloneSetup(leagueController).build();
+    }
+
+    @Test
+    public void showLeagueTest() throws Exception {
+        League league = new League();
+        League league1 = new League();
+        Set<League> leagues = new HashSet<>();
+        league.setId(1L);
+        league1.setId(2L);
+        leagues.add(league);
+        leagues.add(league1);
+
+        when(leagueService.getLeagues()).thenReturn(leagues);
+
+        ArgumentCaptor<Set> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        String viewName = leagueController.showLeague(model);
+
+        assertEquals("league/index", viewName);
+        verify(leagueService, times(1)).getLeagues();
+        verify(model, times(1)).addAttribute(eq("leagues"), argumentCaptor.capture());
+
+        assertEquals(2, leagues.size());
+
+    }
+
+    @Test
+    public void showLeagueById() throws Exception {
+        League league = new League();
+        league.setId(1L);
+
+        when(leagueService.getLeagueById(anyLong())).thenReturn(league);
+
+        mockMvc.perform(get("/leagues/1/show"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("/leagues/1/show"));
+    }
+
+    @Test
+    public void newLeague() throws Exception {
+
+        mockMvc.perform(get("/league/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("league/show"));
+    }
+
+    @Test
+    public void deleteLeague() throws Exception {
+
+        mockMvc.perform(get("/league/1/delete"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/show"));
+
+        verify(leagueService, times(1)).deleteLeagueById(anyLong());
+    }
+
+    @Test
+    public void saveOrUpdateLeague() throws Exception {
+        League league = new League();
+        league.setId(1L);
+
+        when(leagueService.getLeagueById(anyLong())).thenReturn(league);
+
+        mockMvc.perform(get("/league/1/update"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("league/leagueform"));
+
+    }
+
+}
