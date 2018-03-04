c7d8fa2ece0688cd0b4c82597a518a7ebfccead0
terrato92 <kamilmituraa@gmail.com>
2018-03-04 17:04:50 +0100

    player commit

--- src/main/java/terrato/springframwork/service/PlayerService.java
+++ src/main/java/terrato/springframwork/service/PlayerService.java
@@ -1,7 +1,26 @@
 package terrato.springframwork.service;
 
+import terrato.springframwork.domain.Player;
+
+import java.util.Collection;
+
 /**
  * Created by onenight on 2018-03-03.
  */
 public interface PlayerService {
+
+    Player createPlayer(Player playerSource);
+
+    Collection<Player> getPlayers();
+
+    Collection<Player> getPlayersFromTeam(Long idTeam);
+
+
+    Collection<Player> addPlayerToTeam(Player player, Long idTeam);
+
+
+    void deletePlayer(Long idPlayer);
+
+    void deletePlayerFromTeam(Long idPlayer, Long idTeam);
+
 }
--- src/main/java/terrato/springframwork/service/implementation/PlayerServiceImpl.java
+++ src/main/java/terrato/springframwork/service/implementation/PlayerServiceImpl.java
@@ -1,9 +1,118 @@
 package terrato.springframwork.service.implementation;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import terrato.springframwork.domain.Player;
+import terrato.springframwork.domain.Team;
+import terrato.springframwork.repository.PlayerRepository;
+import terrato.springframwork.repository.TeamRepository;
 import terrato.springframwork.service.PlayerService;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * Created by onenight on 2018-03-03.
  */
+@Service
+@Slf4j
 public class PlayerServiceImpl implements PlayerService {
+
+    PlayerRepository playerRepository;
+    TeamRepository teamRepository;
+
+
+    @Override
+    public Player createPlayer(Player playerSource) {
+        Optional<Player> playerOptional = Optional.ofNullable(playerRepository.findOne(playerSource.getId()));
+
+        if (!playerOptional.isPresent()) {
+            Player player = playerOptional.get();
+
+            player.setName(playerSource.getName());
+            player.setAge(playerSource.getAge());
+            if (playerSource.getTeam() != null) {
+                player.setTeam(playerSource.getTeam());
+            } else {
+                player.setTeam(null);
+            }
+            player.setPosition(playerSource.getPosition());
+
+            playerRepository.save(player);
+
+            return player;
+        } else {
+            log.error("Player with id: " + playerSource.getId() + "already exist.");
+            throw new RuntimeException("You can't add player with the same id");
+        }
+    }
+
+    @Override
+    public Collection<Player> getPlayers() {
+        Set<Player> playerSet = new HashSet<>();
+        playerRepository.findAll().iterator().forEachRemaining(playerSet::add);
+        return playerSet;
+    }
+
+    @Override
+    public Collection<Player> getPlayersFromTeam(Long idTeam) {
+        Optional<Team> teamOptional = Optional.ofNullable(teamRepository.findOne(idTeam));
+
+        if (teamOptional.isPresent()) {
+            return teamOptional.get().getPlayers();
+        } else {
+            log.error("Team with id: " + idTeam + " doesn't exist");
+            throw new RuntimeException("I can't find team with id: " + idTeam);
+        }
+    }
+
+    @Override
+    public Collection<Player> addPlayerToTeam(Player player, Long idTeam) {
+        Optional<Team> teamOptional = Optional.ofNullable(teamRepository.findOne(idTeam));
+
+        if (teamOptional.isPresent()) {
+            Team team = teamOptional.get();
+
+            player.setTeam(team);
+            team.getPlayers().add(player);
+
+            playerRepository.save(player);
+            teamRepository.save(team);
+
+            return team.getPlayers();
+        } else {
+            log.error("Team with id: " + idTeam + " doesn't exist");
+            throw new RuntimeException("I can't find team with id: " + idTeam);
+        }
+    }
+
+    @Override
+    public void deletePlayer(Long idPlayer) {
+        Optional<Player> playerOptional = Optional.ofNullable(playerRepository.findOne(idPlayer));
+
+        if (playerOptional.isPresent()) {
+            playerRepository.delete(idPlayer);
+        }
+    }
+
+    @Override
+    public void deletePlayerFromTeam(Long idPlayer, Long idTeam) {
+        Optional<Team> teamOptional = Optional.ofNullable(teamRepository.findOne(idPlayer));
+
+        if (teamOptional.isPresent()) {
+            Team team = teamOptional.get();
+
+            Optional <Player> playerOptional = team.getPlayers().stream()
+                    .filter(player -> player.getId().equals(idPlayer))
+                    .findFirst();
+
+            Player playerToDelete = playerOptional.get();
+
+            team.getPlayers().remove(playerToDelete);
+
+            teamRepository.save(team);
+        }
+    }
 }
